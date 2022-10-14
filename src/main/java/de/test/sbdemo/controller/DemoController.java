a2c5b9acbae4361b77c4001401fc5fdf068a886c
Ralf <ra64kon@googlemail.com>
2022-10-14 13:37:19 +0200

    unicode test

--- src/main/java/de/test/sbdemo/controller/DemoController.java
+++ src/main/java/de/test/sbdemo/controller/DemoController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DemoController {
 
-    private String message = "my dynamic message";
+    char c = '\u2654';
+    private String message = "message: " + c;
     private int count=0;
 
     @RequestMapping("/test")
