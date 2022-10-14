6787dc73bcce958b6b9c7980950c4a81958e39a6
Ralf <ra64kon@googlemail.com>
2022-10-14 10:41:15 +0200

    first commit

---
+++ src/main/java/de/test/sbdemo/controller/DemoController.java
@@ -0,0 +1,26 @@
+package de.test.sbdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class DemoController {
+
+    private String message = "mmmmm";
+    private int count=0;
+
+    @RequestMapping("/test")
+    public String test(Model model){
+        count++;
+        model.addAttribute("message", message);
+        model.addAttribute("count", count);
+        return "test";
+    }
+
+    @RequestMapping("/")
+    public String welcome(){
+        return "welcome";
+    }
+}
